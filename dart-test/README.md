eb1bcc1a81c3d2155daeb22b7629d489f7dd261c
Igor Borges <igor@borges.dev>
2020-02-01 18:21:12 -0300

    Revert change and create flutter flag

--- dart-test/README.md
+++ dart-test/README.md
@@ -6,20 +6,8 @@ Test, analyze and lint a Dart/Flutter package.
 
 | Required? | Variable                 | Description                                                               |
 |-----------|--------------------------|---------------------------------------------------------------------------|
+| No        | **DTA_IS_FLUTTER**       | Run everything on Flutter context instead of Dart's                       |
 | No        | **DTA_DISABLE_LINTER**   | Disable dry-run `dartfmt` linter check                                    |
 | No        | **DTA_DISABLE_ANALYZER** | Disable `dartanalyzer`                                                    |
 | No        | **DTA_DISABLE_TESTS**    | Disable `pub run test`                                                    |
 | No        | **DTA_EXCLUDE_REGEX**    | Regex to be used to exclude folders when looking for `pubspec.yaml` files |
-
-## Example workflow
-
-```hcl
-workflow "Test my package!" {
-  on = "push"
-  resolves = ["Test"]
-}
-
-action "Test" {
-  uses = "comigor/actions/dart-test@master"
-}
-```
--- dart-test/entrypoint.sh
+++ dart-test/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eu
 
+echo "DTA_IS_FLUTTER=$DTA_IS_FLUTTER"
 echo "DTA_DISABLE_LINTER=$DTA_DISABLE_LINTER"
 echo "DTA_DISABLE_ANALYZER=$DTA_DISABLE_ANALYZER"
 echo "DTA_DISABLE_TESTS=$DTA_DISABLE_TESTS"
@@ -12,27 +13,35 @@ for ppath in $(find . -name pubspec.yaml | grep -ve "$DTA_EXCLUDE_REGEX"); do
   echo "=== On $ppath ==="
   cd $(dirname "$ppath");
 
-  [ -d "test" ] && {
-    echo "=== Downloading dependencies ==="
+  echo "=== Downloading dependencies ==="
+  if [ -z "$DTA_IS_FLUTTER" ]; then
+    pub get
+  else
     flutter pub get
+  fi
 
-    if [ -z "$DTA_DISABLE_LINTER" ]; then
-      echo "=== Running linter ==="
-      dartfmt -n . --set-exit-if-changed
+  if [ -z "$DTA_DISABLE_LINTER" ]; then
+    echo "=== Running linter ==="
+    dartfmt -n . --set-exit-if-changed
 
-      if [ $? -ne 0 ]; then
-        echo "Linter failed. Check the files above."
-      fi
+    if [ $? -ne 0 ]; then
+      echo "Linter failed. Check the files above."
     fi
+  fi
 
-    if [ -z "$DTA_DISABLE_ANALYZER" ]; then
-      echo "=== Running analyzer ==="
-      dartanalyzer --fatal-infos --fatal-warnings .
-    fi
+  if [ -z "$DTA_DISABLE_ANALYZER" ]; then
+    echo "=== Running analyzer ==="
+    dartanalyzer --fatal-infos --fatal-warnings .
+  fi
 
+  [ -d "test" ] && {
     if [ -z "$DTA_DISABLE_TESTS" ]; then
       echo "=== Running tests ==="
-      flutter pub run test
+      if [ -z "$DTA_IS_FLUTTER" ]; then
+        pub run test
+      else
+        flutter test
+      fi
     fi
   }
 
