a0b1a5cb10e831f64e3844c4db7cddef55f225f4
Igor Borges <igor@borges.dev>
2021-08-16 22:29:33 -0300

    Add mask for all files in .env

--- git-secret/entrypoint.sh
+++ git-secret/entrypoint.sh
@@ -6,6 +6,10 @@ echo "$INPUT_GPG_KEY" > /tmp/private.key
 gpg --allow-secret-key-import --import /tmp/private.key
 shred -fu /tmp/private.key
 
-# gpg2 -K
+gpg2 -K
 
 git secret reveal
+
+if [ -f ".env" ]; then
+    cat .env | grep -E '^[^#]' | sed -E 's/^[^=#]+=(.*)/\1/g' | xargs -I {} echo "::add-mask::{}"
+fi
