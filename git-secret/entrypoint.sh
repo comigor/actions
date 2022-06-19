c7538c03f1facb42000c9b907a02c05733591cd3
Igor Borges <igor@borges.dev>
2022-06-19 19:49:49 -0300

    Update entrypoint.sh

--- git-secret/entrypoint.sh
+++ git-secret/entrypoint.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 echo "Decrypting .env"
-echo "$INPUT_GPG_KEY" > /tmp/private.key
+echo "$INPUT_GPG_KEY" | sed -E 's/\\n/\n/g' > /tmp/private.key
 gpg --allow-secret-key-import --import /tmp/private.key
 shred -fu /tmp/private.key
 
@@ -11,7 +11,9 @@ gpg2 -K
 echo "Workspace: $GITHUB_WORKSPACE"
 git config --global --add safe.directory $GITHUB_WORKSPACE
 
-git secret reveal
+pkill -9 gpg-agent
+export GPG_TTY=/dev/console
+git secret reveal -f -v
 
 if [ -f ".env" ]; then
     cat .env | grep -E '^[^#]' | sed -E 's/^[^=#]+=(.*)/\1/g' | xargs -I {} echo "::add-mask::{}"
